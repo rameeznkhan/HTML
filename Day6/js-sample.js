a955e2c6581321d6dab14e0a67aa49dff6033bea
rameez <rameeznkhan@gmail.com>
2018-01-29 00:14:46 +0530

    Added assignments and notes
    Added assignment for responsive Layout
    Added Notes and examples for day 6 and day 7

---
+++ Day6/js-sample.js
@@ -0,0 +1,20 @@
+document.write("Hello world");
+document.getElementById("message").innerHTML="Hello World";
+// alert("Sample Alert");
+console.log("Comes from console");
+document.getElementById("msg").innerHTML="msg Hello World";
+var sample = document.getElementById("msg").innerHTML;
+console.log(sample);
+console.log(0.1 + 0.2);
+
+
+var dt = new Date(); //=>Sun Jan 28 2018 17:38:50 GMT+0530 (IST)
+console.log(dt);
+console.log(dt.getDate());
+console.log(dt.getDay());
+console.log(dt.getFullYear());
+console.log(dt.getMonth());
+console.log(dt.getHours());
+console.log(dt.getMinutes());
+console.log(dt.getMilliseconds());
+console.log(dt.getTime());
